5e7ed054acef6118644684b29a67390719e0ffa2
Enisel Kunch <enisel_12@abv.bg>
2025-02-10 01:02:31 +0200

    Register new users

--- ecom/ecom/store/views.py
+++ ecom/ecom/store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm #we are saying "Take it from forms.py" and allow us to use it in our views.py"
+from django import forms
 
 
 def home(request):
@@ -35,3 +39,24 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You have been logged out!"))
     return redirect('home') #we want to send user somewhere after he logged out
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #log in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have registered successfully! Welcome!"))
+            return redirect('home')
+        
+        else:
+            messages.success(request, ("There was a problem registering, please try again!"))
+            return redirect('register')
+
+    else:
+        return render(request, 'register.html', {'form':form}) #just show the page
