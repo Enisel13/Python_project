2efdccc9e16b41845767f64b8eb5039e4fee24b6
Enisel Kunch <enisel_12@abv.bg>
2025-02-10 16:58:03 +0200

    Shopping cart setup

---
+++ ecom/ecom/cart/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+# . означава, че се взима от текущата директория в случая текущата директория е store
+from . import views
+
+urlpatterns = [
+    path('', views.cart_summary, name="cart_summary"),
+    path('add/', views.cart_add, name="cart_add"),
+    path('delete/', views.cart_delete, name="cart_delete"),
+    path('update/', views.cart_update, name="cart_update"),
+]
