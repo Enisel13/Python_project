d54712509f6773fdddd416d4e298652bb5d6d207
Enisel Kunch <enisel_12@abv.bg>
2025-02-11 00:44:12 +0200

    Add product to cart

---
+++ ecom/ecom/cart/cart.py
@@ -0,0 +1,27 @@
+
+class Cart():
+    #request - anytime a user goes to the website he is making a request to view that website 
+    def __init__(self, request):
+        self.session = request.session
+
+        #Get the current session key if it exist
+        cart = self.session.get('session_key') #if the session key exist get it and ssign it to this cart variable
+
+        #If the user is new, no session key, create one
+        if 'session_key' not in request.session:
+            cart = self.session['session_key'] = {}
+
+        #Make sure cart is available on all pages of site
+        self.cart = cart
+
+    def add(self, product):
+        product_id = str(product.id)
+
+        #Logic
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+
+        self.session.modified = True
+        
