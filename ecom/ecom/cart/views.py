505b7688a0bf68e2fa48bf86fd1e6539c6c69948
Enisel Kunch <enisel_12@abv.bg>
2025-02-12 18:04:55 +0200

    Shopping cart messages

--- ecom/ecom/cart/views.py
+++ ecom/ecom/cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
 
 # Create your views here.
 #The function we call every time when we open a summary page for cart
@@ -30,6 +31,7 @@ def cart_add(request):
         #Return response
         #response = JsonResponse({'Product name: ': product.name})
         response = JsonResponse({'qty': cart_quantity})
+        messages.success(request, ("Product added to cart"))
         return response
 
 def cart_delete(request):
@@ -41,6 +43,7 @@ def cart_delete(request):
         cart.delete(product=product_id)
 
         response = JsonResponse({'product':product_id})
+        messages.success(request, ("The item was deleted from shopping cart!"))
         return response
         #return redirect('cart_summary') може и така
 
@@ -54,5 +57,6 @@ def cart_update(request):
         cart.update(product=product_id, quantity=product_qty)
 
         response = JsonResponse({'qty':product_qty})
+        messages.success(request, ("Your cart has been updated! "))
         return response
         #return redirect('cart_summary') може и така
